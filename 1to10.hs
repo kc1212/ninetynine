cb6c6547ba9145e7f9982cb197a56c1530b77b11
Kelong Cong <kelongcong@gmail.com>
2014-05-13 22:02:00 +0100

    git a few more list questions

--- 1to10.hs
+++ 1to10.hs
@@ -12,4 +12,32 @@ myButLast [a,_] = a
 myButLast (_:xs) = myButLast xs
 
 -- 3
--- elementAt :: [a] -> a
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "Element does not exist!"
+elementAt (x:xs) 1 = x
+elementAt (x:xs) i = elementAt xs (i-1)
+
+-- 4
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = (+1) $ myLength xs
+
+-- 5
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- 6
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [x] = True
+isPalindrome xs =
+	h == l && isPalindrome m
+	where
+		h = head xs
+		l = last xs
+		m = init $ tail xs
+-- or just check the reverse
+
+
+
